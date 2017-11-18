323d880b7cc783b4651a32cc7e13aa9cfd0f6943
Tristan Remy <tristan.remy1@gmail.com>
2017-11-18 17:37:11 +0100

    modifications maison tr

--- post-install.sh
+++ post-install.sh
@@ -40,74 +40,24 @@ brew tap caskroom/cask
 
 ## Installations des logiciels
 echo 'Installation des outils en ligne de commande.'
-brew install wget cmake coreutils psutils git ffmpeg libssh zsh vim git-extras java sshfs boost opencv htop
-brew tap zyedidia/micro
-brew install micro bower xmlstarlet
-gem install sass
+brew install git ffmpeg libssh sshfs opencv htop noe python3 yarn
 
 # Installer oh My zsh & met ZSH par default
-sudo curl -L http://install.ohmyz.sh | sh
-chsh -s /bin/zsh
-git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
+brew install fish
+chsh -s `which fish`
 
 
-echo 'Installation des apps : utilitaires.'
-brew tap homebrew/science
-brew cask install google-drive  dropbox  slack hyperdock sublime-text osxfuse java webstorm clion
-install "FastScripts"
-install "MacTracker"
+echo 'Installation des apps via brew cask'
+brew cask install alfred caption dashlane docker-toolbox firefox franz google-backup-and-sync google-chrome handbrake handbrakebatch hazel imageoptim insomnia iterm2 kap macs-fan-control  molotov muzzle ngrok numi openoffice sketch spotify the-unarchiver trailer transmit transmission tunnelbear tweeten viscosity visual-studio-code vlc zoom
 
 ## Add theme to oh-my-zsh
-cp bullet-train.zsh-theme ~/.oh-my-zsh/themes
+curl -L https://get.oh-my.fish | fish
 
 ## Install police  powerfont for terminal
 sh ./fonts-master/install.sh
 
 #echo "Ouverture de Google Drive pour commencer la synchronisation"
 open -a Google\ Drive
-
-# Installation manuelle de SearchLink
-cd /tmp/ && curl -O http://cdn3.brettterpstra.com/downloads/SearchLink2.2.3.zip && unzip SearchLink2.2.3.zip && cd SearchLink2.2.3 && mv SearchLink.workflow ~/Library/Services/
-
-echo 'Installation des apps : bureautique.'
-install "Marked"
-easy_install SpoofMAC
-
-## INSTALL SUBLIME 3 VERSION
-defaults write com.apple.LaunchServices LSHandlers -array-add '{LSHandlerContentType=public.plain-text;LSHandlerRoleAll=com.sublimetext.3;}'
-brew cask install sublime-text
-## INSTALL SUBLIME PACKAGE MANAGEMENT
-wget https://sublime.wbond.net/Package%20Control.sublime-package
-mv Package\ Control.sublime-package ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/
-
-echo 'Installation des apps : développement.'
-brew cask install install "docker"
-
-
-echo 'Installation des apps : communication.'
-brew cask install google-chrome firefox
-brew cask install chromecast --appdir=/Applications && defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool false
-brew cask install silverlight flash-player --appdir=/Applications
-
-
-#echo 'Installation des apps : photo et vidéo.'
-#brew cask install imageoptim sketch qlimagesize
-
-echo 'Installation des apps : loisir.'
-brew cask install vlc the-unarchiver spotify --appdir=/Applications
-
-# DockArt (installation manuelle, faute de mieux)
-cd /tmp/ && curl -O http://www.splook.com/Software/DockArt_files/DockArt2.zip && unzip DockArt2.zip && cd DockArt\ 2.2 && mv DockArt.bundle ~/Library/iTunes/iTunes\ Plug-ins
-
-
-## Install de tesseract & quelques Library
-brew install htop
-brew install leptonica --with-libtiff
-brew install imagemagick
-brew install tesseract --with-all-languages
-brew tap homebrew/science
-brew install boost
-brew install opencv
 
 ## ************************* CONFIGURATION ********************************
 echo "Configuration de quelques paramètres par défaut…"
@@ -152,36 +102,9 @@ defaults write com.apple.finder AppleShowAllFiles -bool true
 # Rend les icones des app cachés transluicides
 defaults write com.apple.dock showhidden -bool true
 
-
-## RÉGLAGES DOCK
-# Taille du texte au minimum
-#defaults write com.apple.dock tilesize -int 15
-# Agrandissement actif
-defaults write com.apple.dock magnification -bool true
-# Taille maximale pour l'agrandissement
-defaults write com.apple.dock largesize -float 90
-
-## MISSION CONTROL
-# Pas d'organisation des bureaux en fonction des apps ouvertes
-defaults write com.apple.dock mru-spaces -bool false
-
 # Mot de passe demandé immédiatement quand l'économiseur d'écran s'active
 defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
-
-## COINS ACTIFS
-# En haut à gauche : bureau
-#defaults write com.apple.dock wvous-tl-corner -int 4
-#defaults write com.apple.dock wvous-tl-modifier -int 0
-# En haut à droite : bureau
-#defaults write com.apple.dock wvous-tr-corner -int 4
-#defaults write com.apple.dock wvous-tr-modifier -int 0
-# En bas à gauche : fenêtres de l'application
-#defaults write com.apple.dock wvous-bl-corner -int 3
-#defaults write com.apple.dock wvous-bl-modifier -int 0
-# En bas à droite : Mission Control
-#defaults write com.apple.dock wvous-br-corner -int 2
-#defaults write com.apple.dock wvous-br-modifier -int 0
 
 ## CLAVIER ET TRACKPAD
 
@@ -210,18 +133,15 @@ sudo defaults write NSGlobalDomain KeyRepeat -int 1
 sudo defaults write NSGlobalDomain InitialKeyRepeat -int 10
 
 # Alertes sonores quand on modifie le volume
-sudo defaults write ~/Library/Preferences/.GlobalPreferences.plist -int 1
+sudo defaults write ~/Library/Preferences/.GlobalPreferences.plist -int 0
 
 # Réglages Trackpad : toucher pour cliquer
-#sudo defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
-#sudo defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+sudo defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+sudo defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 
 # Trier l'activité du moniteur par CPU
 defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
 defaults write com.apple.ActivityMonitor SortDirection -int 0
-
-# Desactivation des DASHBOARD
-defaults write com.apple.dashboard mcx-disabled -boolean YES
 
 # Desactivation du fast User switch
 sudo defaults write /Library/Preferences/.GlobalPreferences MultipleSessionEnabled -bool false
@@ -229,15 +149,11 @@ sudo defaults write /Library/Preferences/.GlobalPreferences MultipleSessionEnabl
 # Desactive TimeMachien sur les nouveaux volumes
 defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
 
-
 ## Activation de debug dans utilitaire de disque
 defaults write com.apple.DiskUtility DUDebugMenuEnabled -bool true
 defaults write com.apple.DiskUtility advanced-image-options -bool true
 
 ## APPS
-
-# Désactivation de l'autocorrection
-defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 
 # Safari : menu développeur / URL en bas à gauche / URL complète en haut / Do Not Track / affichage barre favoris
 defaults write com.apple.safari IncludeDevelopMenu -int 1
@@ -246,7 +162,7 @@ defaults write com.apple.safari ShowFullURLInSmartSearchField -int 1
 defaults write com.apple.safari SendDoNotTrackHTTPHeader -int 1
 defaults write com.apple.Safari ShowFavoritesBar -bool true
 
-# Desactivationd es miniatures safari
+# Desactivation des miniatures safari
 defaults write com.apple.Safari DebugSnapshotsUpdatePolicy -int 2
 
 # Activation du retour arriére avec backspace safari
@@ -255,37 +171,8 @@ defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebK
 # Photos : pas d'affichage pour les iPhone
 defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool YES
 
-
 # TextEdit : .txt par défaut
 defaults write com.apple.TextEdit RichText -int 0
-
-# Raccourci pour exporter
-sudo defaults write -g NSUserKeyEquivalents '{"Export…"="@$e";"Exporter…"="@$e";}'
-
-# Configuration de git
-git_configs=(
-  "branch.autoSetupRebase always"
-  "color.ui auto"
-  "core.autocrlf input"
-  "core.pager cat"
-  "credential.helper osxkeychain"
-  "merge.ff false"
-  "pull.rebase true"
-  "push.default simple"
-  "rebase.autostash true"
-  "rerere.autoUpdate true"
-  "rerere.enabled true"
-  "user.name MatthD"
-  "user.email matthias.dieudonne@gmail.com"
-)
-
-for config in "${git_configs[@]}"
-do
-  git config --global ${config}
-done
-
-echo "Installing mac CLI ..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/guarinogabriel/mac-cli/master/mac-cli/tools/install)"
 
 ## ************ Fin de l'installation *********
 echo "Finder et Dock relancés… redémarrage nécessaire pour terminer."
@@ -297,7 +184,7 @@ echo "Derniers nettoyages…"
 brew cleanup
 rm -f -r /Library/Caches/Homebrew/*
 
-echo "ET VOILÀ !"
+echo "Installation terminée !"
 
 # Active le trimeForce (Pour les SSD)
 echo "Le trimeforce va etre activé pour votre SSD , un redemarage est requis, executez script post-cloud.sh aprés le redemarage"
